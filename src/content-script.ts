bd58bf5a210c78d00735d3a592a6de677b8e0bd5
Jake Hein <jacobnolanhein@gmail.com>
2022-11-24 01:01:45 -0800

    back at it. http get, testing

--- src/content-script.ts
+++ src/content-script.ts
@@ -2,114 +2,211 @@
 // https://yugioh.fandom.com/wiki/Gallery_of_Yu-Gi-Oh!_The_Eternal_Duelist_Soul_cards
 
 enum BoosterPack {
-  DarkMagician = "Dark Magician",
-  MysticalElf = "Mystical Elf",
-  RedEyesBDragon = "Red-Eyes B. Dragon",
-  JudgeMan = "Judge Man",
-  BlueEyesWhiteDragon = "Blue-Eyes White Dragon",
-  Exodia = "Exodia",
-  CyberHarpie = "Cyber Harpie",
-  TigerAxe = "Tiger Axe",
-  GateGuardian = "Gate Guardian",
-  GeminiElf = "Gemini Elf",
-  LauncherSpider = "Launcher Spider",
-  GreatMoth = "Great Moth",
-  Garoozis = "Garoozis",
-  Relinquished = "Relinquished",
-  BattleOx = "Battle Ox",
-  BlueEyesToonDragon = "Blue-Eyes Toon Dragon",
-  BlackLusterSoldier = "Black Luster Soldier",
-  BlueEyesUltimateDragon = "Blue-Eyes Ultimate Dragon",
-  BlueMilleniumPuzzle = "Blue Millenium Puzzle",
-  MilleniumEye = "Millenium Eye",
-  BusterBlader = "Buster Blader",
-  GreenMilleniumPuzzle = "Green Millenium Puzzle",
-  MultiColoredMilleniumPuzzle = "Multi-Colored Millenium Puzzle",
-};
+	DarkMagician = 'Dark Magician',
+	MysticalElf = 'Mystical Elf',
+	RedEyesBDragon = 'Red-Eyes B. Dragon',
+	JudgeMan = 'Judge Man',
+	BlueEyesWhiteDragon = 'Blue-Eyes White Dragon',
+	Exodia = 'Exodia',
+	CyberHarpie = 'Cyber Harpie',
+	TigerAxe = 'Tiger Axe',
+	GateGuardian = 'Gate Guardian',
+	GeminiElf = 'Gemini Elf',
+	LauncherSpider = 'Launcher Spider',
+	GreatMoth = 'Great Moth',
+	Garoozis = 'Garoozis',
+	Relinquished = 'Relinquished',
+	BattleOx = 'Battle Ox',
+	BlueEyesToonDragon = 'Blue-Eyes Toon Dragon',
+	BlackLusterSoldier = 'Black Luster Soldier',
+	BlueEyesUltimateDragon = 'Blue-Eyes Ultimate Dragon',
+	BlueMilleniumPuzzle = 'Blue Millenium Puzzle',
+	MilleniumEye = 'Millenium Eye',
+	BusterBlader = 'Buster Blader',
+	GreenMilleniumPuzzle = 'Green Millenium Puzzle',
+	MultiColoredMilleniumPuzzle = 'Multi-Colored Millenium Puzzle',
+}
 
 enum Rarity {
-  Rare = "Rare",
-  Common = "Common",
+	Rare = 'Rare',
+	Common = 'Common',
 }
 
 window.onload = () => {
-  const button = document.createElement('button');
-  button.id = 'scrapePage';
-  button.textContent = 'Scrape Page';
+	const button = document.createElement('button');
+	button.id = 'scrapePage';
+	button.textContent = 'Scrape Page';
 
-  document.querySelector("#firstHeading").parentElement.append(button);
+	document.querySelector('#firstHeading').parentElement.append(button);
 
-  button.addEventListener('click', () => {
-    //scrapePage();
-    scrapeBoosterPage();
-    button.textContent = "Page Scraped!";
-    button.disabled = true;
-  });
+	button.addEventListener('click', () => {
+		//scrapePage();
+		scrapeBoosterPage();
+		button.textContent = 'Page Scraped!';
+		button.disabled = true;
+	});
 };
 
+// FOR TESTING...
+// https://yugioh.fandom.com/wiki/Dark_Magician_(EDS-BP)
 function scrapeBoosterPage() {
-  // booster pack name
-  const boosterNameElement = document.querySelector('#firstHeading') as HTMLElement;
-  const boosterName = boosterNameElement.innerText.replace('(EDS-BP)', '').trim();
+	// booster pack name
+	const boosterNameElement = document.querySelector(
+		'#firstHeading',
+	) as HTMLElement;
+	const boosterName = boosterNameElement.innerText
+		.replace('(EDS-BP)', '')
+		.trim();
 
-  // booster unlock condition
-  const xpathUnlockCondition = '//th[contains(text(), "Introduction")]/../following-sibling::tr/td/p';
-  const unlockCondition = (evaluateElement(xpathUnlockCondition) as HTMLElement)?.innerText;
-  //const boosterUnlockConditionElement = document.querySelector() as HTMLElement;
-  const img = document.querySelector('div > figure > a.image') as HTMLAnchorElement;
-  const imgLink = img.href
-  console.log(
-    boosterName, '\n',
-    unlockCondition, '\n',
-    imgLink, '\n',
-  )
+	// booster unlock condition
+	const xpathUnlockCondition =
+		'//th[contains(text(), "Introduction")]/../following-sibling::tr/td/p';
+	const unlockCondition = (
+		evaluateElement(document, xpathUnlockCondition) as HTMLElement
+	)?.innerText;
+	//const boosterUnlockConditionElement = document.querySelector() as HTMLElement;
+	const img = document.querySelector(
+		'div > figure > a.image',
+	) as HTMLAnchorElement;
+	const imgLink = img.href;
+	//TODO: need to get the rarity of the card from the page here
+	const cardRarity: string = 'Rare';
+	//TODO: need to get the actual card URL here
+	const cardURL: string = 'https://yugioh.fandom.com/wiki/Beast_Fangs';
+
+	if (!Object.values(Rarity).includes(cardRarity as Rarity)) {
+		alert(
+			`Rarity ${cardRarity} is not an accepted Rarity type. Big oops on the devs part. Bad Dev, Bad!`,
+		);
+		return;
+	} else if (!Object.values(BoosterPack).includes(boosterName as BoosterPack)) {
+		alert(
+			`BoosterPack ${boosterName} is not an accepted BoosterPack type. Big oops on the devs part. Bad Dev, Bad!`,
+		);
+		return;
+	}
+
+	//TODO: get first element in list of cards and get info from card?
+	//BOOSETER TEST
+	getCardInfo(cardURL, boosterName as BoosterPack, cardRarity as Rarity);
+
+	console.log(boosterName, '\n', unlockCondition, '\n', imgLink, '\n');
 }
 
 // TODO: Thinking I'll hit this from the booster pack page, passing in the pack info to make a card
 // The cardID might be "boosterNameCardName" or something
-function scrapePage(boosterPack: BoosterPack, rarity: Rarity) {
-  //get Card Name from header of DOM
-  const cardNameElement = (document.querySelector('#firstHeading') as HTMLElement);
-  const cardName = cardNameElement ? cardNameElement.innerText : -1;
-  //(cardName as string).replace('(EDS-BP)', '').trim();
+function scrapePage(
+	pageDocument: Document,
+	boosterPack: BoosterPack,
+	rarity: Rarity,
+) {
+	//get Card Name from header of DOM
+	const cardNameElement = pageDocument.querySelector(
+		'#firstHeading',
+	) as HTMLElement;
+	const cardName = cardNameElement ? cardNameElement.innerText.trim() : -1;
+	//(cardName as string).replace('(EDS-BP)', '').trim();
 
-  //get Card Description from DOM
-  const xpathDescription = '//b[contains(text(),"Card descriptions")]/..//*/tbody/tr[1]/th/div/i/a[contains(text(),"The Eternal Duelist Soul")]';
-  const descriptionNode = (evaluateElement(xpathDescription) as Element)?.closest('tbody');
-  const cardDescriptionElement = (descriptionNode?.querySelector('tr:nth-child(3) > td') as HTMLElement);
-  const cardDescription = cardDescriptionElement ? cardDescriptionElement.innerText : -1;
+	//get Card Description from DOM
+	const xpathDescription =
+		'//b[contains(text(),"Card descriptions")]/..//*/tbody/tr[1]/th/div/i/a[contains(text(),"The Eternal Duelist Soul")]';
+	const descriptionNode = (
+		evaluateElement(pageDocument, xpathDescription) as Element
+	)?.closest('tbody');
+	const cardDescriptionElement = descriptionNode?.querySelector(
+		'tr:nth-child(3) > td',
+	) as HTMLElement;
+	const cardDescription = cardDescriptionElement
+		? cardDescriptionElement.innerText.trim()
+		: -1;
 
-  // get card type node on DOM
-  var xpathCardType = '//a[contains(text(),"Card type")]/../following-sibling::td/a';
-  const cardTypeElement = (evaluateElement(xpathCardType) as HTMLElement);
-  let cardType = cardTypeElement ? cardTypeElement.innerText : -1;
+	// get card type node on DOM
+	var xpathCardType =
+		'//a[contains(text(),"Card type")]/../following-sibling::td/a';
+	const cardTypeElement = evaluateElement(
+		pageDocument,
+		xpathCardType,
+	) as HTMLElement;
+	let cardType = cardTypeElement ? cardTypeElement.innerText.trim() : -1;
 
+	// console log everything
+	console.log(cardName);
+	console.log(cardDescription);
+	console.log(boosterPack);
+	console.log(cardType);
+	console.log(rarity);
+}
 
-  // console log everything
-  console.log(
-    cardName, '\n',
-    cardDescription, '\n',
-    // cardboosterPacks, '\n', 
-    cardType);
-};
+//TODO: decide if this should be called once per string or pass all strings of card into this one time
+function errorHandleStrings(
+	cardName: string | number,
+	cardDescription: string | number,
+	cardType: string | number,
+): {
+	cardName: string;
+	cardDescription: string;
+	cardType: string;
+} {
+	//TODO: if str is -1, say what the card was and what got goofed
+	if (cardName === -1 || cardDescription === -1 || cardType === -1) {
+		console.log(
+			'Error with card: ',
+			cardName,
+			' description: ',
+			cardDescription,
+			' cardType: ',
+			cardType,
+		);
+	}
 
-function evaluateElement(xpath: string) {
-  return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+	return {
+		cardName: (cardName as string).trim(),
+		cardDescription: (cardDescription as string).trim(),
+		cardType: (cardType as string).trim(),
+	};
+}
+
+function evaluateElement(pageDocument: Document, xpath: string) {
+	return pageDocument.evaluate(
+		xpath,
+		pageDocument,
+		null,
+		XPathResult.FIRST_ORDERED_NODE_TYPE,
+		null,
+	).singleNodeValue;
+}
+
+function getCardInfo(
+	url: RequestInfo | URL,
+	boosterPackName: BoosterPack,
+	rarity: Rarity,
+) {
+	fetch(url)
+		.then((resp) => resp.text())
+		.then((result) => {
+			//console.log(result);
+			scrapePage(
+				new DOMParser().parseFromString(result, 'text/html'),
+				boosterPackName,
+				rarity,
+			);
+		})
+		.catch((e) => alert(`Card Info could not be retrieved: ${e}`));
 }
 
 function downloadImage(url: RequestInfo | URL, name: string) {
-  fetch(url)
-    .then(resp => resp.blob())
-    .then(blob => {
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.style.display = 'none';
-      a.href = url;
-      // the filename you want
-      a.download = name;
-      document.body.appendChild(a);
-      a.click();
-      window.URL.revokeObjectURL(url);
-    })
-    .catch(() => alert('An error sorry'));
+	fetch(url)
+		.then((resp) => resp.blob())
+		.then((blob) => {
+			const url = window.URL.createObjectURL(blob);
+			const a = document.createElement('a');
+			a.style.display = 'none';
+			a.href = url;
+			// the filename you want
+			a.download = name;
+			document.body.appendChild(a);
+			a.click();
+			window.URL.revokeObjectURL(url);
+		})
+		.catch(() => alert('An error sorry'));
 }
